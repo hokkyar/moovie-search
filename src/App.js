3ce1774d750d8c7cf0c5e15c772b7ad175964915
Hokky Aryasta <hokkyaryasta@gmail.com>
2022-11-26 12:03:06 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { getMovies, searchMovie } from './api.js'
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    getMovies().then((results) => {
+      setMovies(results)
+    })
+  }, [])
+
+  const search = async (query) => {
+    if (query.length === 0) {
+      getMovies().then((results) => {
+        setMovies(results)
+      })
+    }
+    if (query.length > 3) {
+      const results = await searchMovie(query)
+      setMovies(results)
+    }
+  }
+
+  const Movies = () => {
+    return movies.map((movie, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{movie.title}</div>
+          <img src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} alt={movie.title} className="Movie-image" />
+          <div className="Movie-date">Release: {movie.release_date}</div>
+          <div className="Movie-rate">{movie.vote_average}</div>
+        </div>
+      )
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Moovie Search</h1>
+        <input
+          type="text"
+          placeholder='Search movie...'
+          className="Movie-search"
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="Movie-container">
+          <Movies />
+        </div>
       </header>
     </div>
   );
